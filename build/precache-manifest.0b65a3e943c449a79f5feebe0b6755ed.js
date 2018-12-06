8317a9a771f980401f0bb1c12e0a9f56496b4a94
Brian Yates <byates123@gmailcom>
2018-12-06 17:57:50 -0500

    2nd try

---
+++ build/precache-manifest.0b65a3e943c449a79f5feebe0b6755ed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "914ed44cea7eab887bd4f766e7804352",
+    "url": "/static/media/thumbs_up.914ed44c.svg"
+  },
+  {
+    "revision": "5f854386152cc76df200d9bf7685efb2",
+    "url": "/static/media/logo.5f854386.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "69c6f869429fbb30c194",
+    "url": "/static/js/main.69c6f869.chunk.js"
+  },
+  {
+    "revision": "29f976eee5d55b6d0e57",
+    "url": "/static/js/1.29f976ee.chunk.js"
+  },
+  {
+    "revision": "69c6f869429fbb30c194",
+    "url": "/static/css/main.467d12b9.chunk.css"
+  },
+  {
+    "revision": "4f22e484a58ffb5b48be2df45d78ae75",
+    "url": "/index.html"
+  }
+];
